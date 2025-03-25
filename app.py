502dcdb43cade84a94a169581332dd66d0089413
Peace3B <p.keza@alustudent.com>
2025-03-25 12:49:28 +0200

    Let every child dream.

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
